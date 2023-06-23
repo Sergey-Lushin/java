228995f3fa8f8f2403f326dac9019a98f2f9789f
Sergey <llan@yandex.ru>
2023-06-23 20:00:52 +0300

    Домашняя работа к 2 семиару

--- Lesson2.java
+++ Lesson2.java
@@ -23,8 +23,6 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Lesson2 {
     public static void main(String[] args) throws IOException {
